ce16b0b0aae7c28270520fee50ac3b5c6a3d843a
andrebq <andrebq@gmail.com>
2020-12-13 19:16:28 +0100

    feat: add the concept of mode and handle char events

--- panels.py
+++ panels.py
@@ -1,6 +1,9 @@
 import wx
 import wx.stc
 
+INSERT_MODE = 'I'
+NAVIGATE_MODE= 'N'
+
 
 class CodeEditorPanel(wx.Panel):
     """CodeEditorPanel wraps all the logic to process user input movement and
@@ -16,9 +19,24 @@ class CodeEditorPanel(wx.Panel):
 
         self.__stc.StyleSetFont(wx.stc.STC_STYLE_DEFAULT,
             wx.Font(wx.FontInfo(14).Family(wx.FONTFAMILY_TELETYPE))) 
-
+        
+        self.__mode = INSERT_MODE
 
         self.__vbox.Add(self.__stc, wx.ID_ANY, wx.EXPAND | wx.ALL, 1)
+        self.__stc.Bind(wx.EVT_CHAR, self.__handleChar, id=wx.ID_ANY)
         self.__stc.SetScrollWidth(self.GetSize().width)
         self.__stc.SetScrollWidthTracking(1)
         self.SetSizer(self.__vbox)
+    
+    def __handleChar(self, event, *args, **kwargs):
+        print(event)
+        if self.__mode == INSERT_MODE:
+            self.__handleCharInsertMode(event, *args, **kwargs)
+        elif self.mode == NAVIGATE_MODE:
+            self.__handleCharNavMode(event, *args, **kwargs)
+
+    def __handleCharNavMode(self, event, *args, **kwargs):
+        pass
+    
+    def __handleCharInsertMode(self, event, *args, **kwargs):
+        event.Skip()
